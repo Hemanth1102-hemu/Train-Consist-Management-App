1d057694a048ce88abf83c64a9a51f07b0cfb5c5
Hemanth1102-hemu <hemanthdevarasetty@gmail.com>
2026-04-02 15:39:06 +0530

    Implement bogie capacity mapping in main method
    Added a main method to manage bogie capacities.

--- src/Train_Consist_Management_App.java
+++ src/Train_Consist_Management_App.java
@@ -1,2 +1,22 @@
-public class Train_Consist_Management_App {
-}
+import java.util.HashMap;
+import java.util.Map;
+
+            public class Train_Consist_Management_App {
+
+                public static void main(String[] args) {
+
+                    HashMap<String, Integer> bogieCapacityMap = new HashMap<>();
+
+                    bogieCapacityMap.put("Sleeper", 72);
+                    bogieCapacityMap.put("AC Chair", 56);
+                    bogieCapacityMap.put("First Class", 40);
+
+                    System.out.println("Bogie Capacity Mapping:");
+
+                    for (Map.Entry<String, Integer> entry : bogieCapacityMap.entrySet()) {
+                        System.out.println(entry.getKey() + " Bogie → Capacity: " + entry.getValue());
+                    }
+
+                    System.out.println("\nTotal Bogies Mapped: " + bogieCapacityMap.size());
+                }
+            }
